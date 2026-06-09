8fb558ac8ca5c3688b4150cf557c5b34a21edd43
Murilo <23004326@uepg.br>
2026-06-09 17:06:59 -0300

    corrigindo parametros favoritos

--- js/favoritos.js
+++ js/favoritos.js
@@ -46,25 +46,23 @@ async function carregarFavoritos() {
 
         lista.innerHTML += `
         <div class="favorito-card">
-
+ 
             <img
                 src="${pokemon.imagem}"
                 alt="${pokemon.nome}"
                 width="80"
             >
-
+ 
             <p>${pokemon.nome}</p>
-
+ 
             <button
-                onclick="adicionarAoTime(
-                    ${pokemon.pokemon_id},
-                    '${pokemon.nome}',
-                    '${pokemon.imagem}'
+                onclick="adicionarAoTimePorFavorito(
+                    ${pokemon.pokemon_id}
                 )"
             >
                 ➕ Time
             </button>
-
+ 
             <button
                 onclick="removerFavorito(
                     ${pokemon.id}
@@ -72,10 +70,61 @@ async function carregarFavoritos() {
             >
                 🗑 Remover
             </button>
-
+ 
         </div>
         `;
     });
+}
+
+// Busca os dados completos na PokeAPI antes de adicionar ao time
+async function adicionarAoTimePorFavorito(pokemon_id) {
+
+    try {
+
+        // Busca os dados completos do pokémon na API
+        const pokemon = await buscarPokemonApi(pokemon_id);
+
+        const hpStat = pokemon.stats.find(
+            s => s.stat.name === "hp"
+        );
+        const hp = hpStat ? hpStat.base_stat : 0;
+
+        const ataque1 =
+            pokemon.moves[0]?.move.name || "Sem ataque";
+
+        const ataque2 =
+            pokemon.moves[1]?.move.name || "Sem ataque";
+
+        let tipoAtaque1 = pokemon.types[0].type.name;
+        let tipoAtaque2 = pokemon.types[0].type.name;
+
+        if (pokemon.moves[0]) {
+            tipoAtaque1 = await buscarTipoAtaque(ataque1);
+        }
+
+        if (pokemon.moves[1]) {
+            tipoAtaque2 = await buscarTipoAtaque(ataque2);
+        }
+
+        await adicionarAoTime(
+            pokemon.id,
+            pokemon.name,
+            pokemon.sprites.front_default,
+            hp,
+            pokemon.types[0]?.type.name,
+            pokemon.types[1]?.type.name || "",
+            ataque1,
+            ataque2,
+            tipoAtaque1,
+            tipoAtaque2
+        );
+
+    } catch (erro) {
+
+        console.error(erro);
+        alert("Erro ao adicionar Pokémon ao time.");
+
+    }
 }
 
 async function removerFavorito(id) {
